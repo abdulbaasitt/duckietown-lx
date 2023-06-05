ea7064063a40c107905b69a019d8f0da7493ee65
Abdulbaasit Sanusi <abdulbaasitsanusi@gmail.com>
2023-06-06 00:00:14 +0100

    Braitenberg Vehicle Exercise attempt/solution 1

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -6,9 +6,12 @@ import numpy as np
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
+    res[:, shape[1]//2:] = 1  # Positive weights to the right half of the image
+    res[:, :shape[1]//2] = -1  # Negative weights to the left half of the image
+
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    # res[100:150, 100:150] = 1
+    # res[300:, 200:] = 1
     # ---
     return res
 
@@ -17,6 +20,8 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    res[:, :shape[1]//2] = 1  # Positive weights to the left half of the image
+    res[:, shape[1]//2:] = -1  # Negative weights to the right half of the image
+    # res[100:150, 100:300] = -1
     # ---
     return res
